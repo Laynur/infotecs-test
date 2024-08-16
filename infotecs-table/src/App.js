e91b3bbc236b4f6e311a676ae58bc9ee4568f46d
Aynur <ainur2205@gmail.com>
2024-08-17 01:06:34 +0500

    Создал компонент Модального окна. Добавил папку со стилями

--- infotecs-table/src/App.js
+++ infotecs-table/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './style/header.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
     </div>
   );
 }
--- infotecs-table/src/components/Header.js
+++ infotecs-table/src/components/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 function Header(){
     return(
         <>
-        <div className="header">
-
-        </div>
+            <div className="header">
+                <label className="header-title">Таблица</label>
+            </div>
         </>
     );
 }
