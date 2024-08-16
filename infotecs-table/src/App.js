15c94a14c0932925de1aad9e6b0cb4e226ee7da9
Aynur <ainur2205@gmail.com>
2024-08-17 02:04:31 +0500

    Сделал выгрузку пользователей. Накинули немного стилей

--- infotecs-table/src/App.js
+++ infotecs-table/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './style/header.css';
+import './style/table.css';
 import Header from './components/Header';
-
+import Table from './components/Table';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Table></Table>
     </div>
   );
 }
--- infotecs-table/src/components/Table.js
+++ infotecs-table/src/components/Table.js
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-function Table(){
-    return(
+function Table() {
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        try {
+            const response = await fetch('https://dummyjson.com/users');
+            if (!response.ok) {
+                throw new Error('Ошибка при загрузке данных');
+            }
+            const data = await response.json();
+            setUsers(data.users);
+            setFilteredUsers(data.users);
+        } catch (error) {
+            console.error(error);
+            alert('Произошла ошибка при загрузке данных пользователей.');
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+
+    return (
         <>
-            <table>
-                
-            </table>
+            <div className="table">
+                <div className="table-header">
+                    <h2>Таблица пользователей</h2>
+                    <div className="table_search">
+                        <label className="table_search_title">Поиск:</label>
+                        <input
+                            type="text"
+                            placeholder="Введите"
+                            name="table_search_input"
+                            className="table_search_input"
+                        />
+                    </div>
+                </div>
+                <div className="table-content">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ФИО</th>
+                                <th>Возраст</th>
+                                <th>Пол</th>
+                                <th>Номер телефона</th>
+                                <th>Адрес</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredUsers.map(user => (
+                                <tr key={user.id}>
+                                    <td>{user.firstName} {user.lastName}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.gender}</td>
+                                    <td>{user.phone}</td>
+                                    <td>{user.address.city}, {user.address.street}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     );
 }
+
 export default Table;
